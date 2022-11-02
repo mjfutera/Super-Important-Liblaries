c15aae1c03082d26ba12982d8bde992a573d02ea
mjfutera <mojasuperfirma@gmail.com>
2022-11-02 15:15:41 +0100

    Update liblary.php

--- liblary.php
+++ liblary.php
@@ -1,7 +1,7 @@
 <?php
     function getDataFromDB($sql, $db_data) {
         $connection = mysqli_connect($db_data['db_host'], $db_data['db_user'], $db_data['db_pass'], $db_data['db_name']);
-        $query_result = mysqli_query($connection, $sql)->fetch_assoc();
+        $query_result = mysqli_query($connection, $sql);
         mysqli_close($connection);
         return  $query_result;
     }
