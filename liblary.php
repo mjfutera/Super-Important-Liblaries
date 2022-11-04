af0abcb5f755f8e340825c6fa3aced628f4d733a
mjfutera <mojasuperfirma@gmail.com>
2022-11-04 13:27:29 +0100

    v. 1.001
    added function which transform result from SQL into array

--- liblary.php
+++ liblary.php
@@ -1,14 +1,17 @@
 <?php
-    function getDataFromDB($sql, $db_data) {
+    function connectDB($sql, $db_data) {
         $connection = mysqli_connect($db_data['db_host'], $db_data['db_user'], $db_data['db_pass'], $db_data['db_name']);
         $query_result = mysqli_query($connection, $sql);
         mysqli_close($connection);
-        return  $query_result;
+        return $query_result;
     }
 
-    function updateDataInDB($sql, $db_data) {
-        $connection = mysqli_connect($db_data['db_host'], $db_data['db_user'], $db_data['db_pass'], $db_data['db_name']);
-        mysqli_query($connection, $sql);
-        mysqli_close($connection);
+    function dbToArray($query_result) {
+        $array = array();
+        while($row = mysqli_fetch_array($query_result, MYSQLI_ASSOC))
+        {
+            array_push($array, $row);
+        }
+        return $array;
     }
 ?>
