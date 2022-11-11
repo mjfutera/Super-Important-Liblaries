fd3299d1d72d273a43f4be997e0e2fdc0fb4a573
mjfutera <mojasuperfirma@gmail.com>
2022-11-11 17:43:47 +0100

    Correct file with email validation

---
+++ scripts.php
@@ -0,0 +1,30 @@
+<?php
+    function connectDB($sql, $db_data) {
+        $connection = mysqli_connect($db_data['db_host'], $db_data['db_user'], $db_data['db_pass'], $db_data['db_name']);
+        $query_result = mysqli_query($connection, $sql);
+        mysqli_close($connection);
+        return $query_result;
+    }
+
+    function dbToArray($query_result) {
+        $array = array();
+        while($row = mysqli_fetch_array($query_result, MYSQLI_ASSOC))
+        {
+            array_push($array, $row);
+        }
+        return $array;
+    }
+
+    function generateKey() {
+        return implode('-', str_split(substr(strtolower(md5(microtime().rand(1000, 9999))), 0, 30), 6));
+    }
+
+    function emailValidator($email) {
+        $regex = "/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i";
+        if (preg_match($regex, $email)==1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+?>
