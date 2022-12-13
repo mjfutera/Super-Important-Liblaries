d4b17b8ea19e9f37950ebe9651dc448c1d773c7b
mjfutera <mojasuperfirma@gmail.com>
2022-12-13 15:41:03 +0100

    v. 2.012
    added connectSQLite() function

--- scripts.php
+++ scripts.php
@@ -57,4 +57,11 @@
         if ($url===NULL) {$url = $_SERVER['REQUEST_URI']; }
         return explode("/", parse_url($url, PHP_URL_PATH));
     }
+
+    function connectSQLite($sql, $file) {
+        $pdo = new PDO('sqlite:'.$file);
+        $statement = $pdo->query($sql);
+        $rows = $statement -> fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
+     }
 ?>
