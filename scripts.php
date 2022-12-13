89d13fe84365a842f684c66475ef322a0514d79e
mjfutera <mojasuperfirma@gmail.com>
2022-12-13 17:37:58 +0100

    v. 2.013
    added functions:
    - polishPostCodeVerifier()
    - polishPostCodeModifier()

--- scripts.php
+++ scripts.php
@@ -63,5 +63,19 @@
         $statement = $pdo->query($sql);
         $rows = $statement -> fetchAll(PDO::FETCH_ASSOC);
         return $rows;
+    }
+
+    function polishPostCodeVerifier($postcode) {
+        $reg1 = '/^\d{2}-\d{3}$/';
+        $reg2 = '/^\d{5}$/';
+        return (preg_match($reg1, $postcode) || preg_match($reg2, $postcode));
+     }
+     
+     function polishPostCodeModifier($postcode) {
+        if(preg_match('/^\d{5}$/', $postcode)) {
+            $newpostcode = str_split($postcode);
+            return $newpostcode[0].$newpostcode[1]."-".$newpostcode[2].$newpostcode[3].$newpostcode[4];
+        }
+        return $postcode;
      }
 ?>
